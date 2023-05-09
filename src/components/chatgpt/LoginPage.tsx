16be00bd940ebcb36705de3b9da76308b2aca832
kn134133 <46958161+kn134133@users.noreply.github.com>
2023-05-09 18:29:16 +0800

    Update LoginPage.tsx

--- src/components/chatgpt/LoginPage.tsx
+++ src/components/chatgpt/LoginPage.tsx
@@ -28,32 +28,32 @@ export const LoginPage = ({
 
   return (
     <div className="flex flex-col justify-center h-[85vh] md:w-1/2 p-4">
-      <h1 className="text-white text-[34px] font-bold">ChatGPT</h1>
+      <h1 className="text-white text-[34px] font-bold"></h1>
       <p className="text-white text-xl">
-        You need to login first use your own key.
+       
       </p>
       <div className="text-white mt-5">
         <div>
-          1. Sign up for the &nbsp;
+       
           <a
             href="https://platform.openai.com/signup"
             target="_blank"
             className="underline"
           >
-            OpenAI Platform.
+            
           </a>
         </div>
         <div>
-          2. Create a new secret key in &nbsp;
+         
           <a
             href="https://platform.openai.com/account/api-keys"
             target="_blank"
             className="underline"
           >
-            Settings → API keys.
+          
           </a>
         </div>
-        <div>3. Copy and paste your API key here:</div>
+        <div></div>
       </div>
       <div className="my-4 flex gap-2 items-center flex-col md:flex-row">
         <Input
